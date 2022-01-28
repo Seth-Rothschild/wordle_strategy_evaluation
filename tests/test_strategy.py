65f4587e17ed88defa5beb9f142dd7ce93004679
Seth Rothschild <seth.j.rothschild@gmail.com>
2022-01-27 21:56:20 -0500

    Add strategy helpers and examples

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -35,11 +35,106 @@ def test_filter_all(game):
 
 
 def test_get_letter_freq(game):
-    apple_freqs = [{"a": 1}, {"p": 1}, {"p": 1}, {"l": 1}, {"e": 1}]
-    assert apple_freqs == st.get_letter_freq(["apple"])
+    apple_expected = [{"a": 1}, {"p": 1}, {"p": 1}, {"l": 1}, {"e": 1}]
+    apple_real = st.get_letter_freq(["apple"])
+    for real, expected in zip(apple_real, apple_expected):
+        for key in expected.keys():
+            assert real[key] == expected[key]
+
     total = st.get_letter_freq(game.words_list)
     assert total[0]["a"] == 141
     assert total[1]["a"] == 304
     assert total[2]["a"] == 307
     assert total[3]["a"] == 163
     assert total[4]["a"] == 64
+
+
+def test_score(game):
+    words_list = game.words_list
+    freqs = st.get_letter_freq(words_list)
+    assert st.score("apple", freqs) == 846
+    assert st.score("pears", freqs) == 879
+    assert st.score("peach", freqs) == 982
+    assert freqs == st.DEFAULT_FREQS
+
+
+def test_sort_dict():
+    assert st._sort_dict({"b": 2, "c": 3}) == {"c": 3, "b": 2}
+
+
+def test_score_words_list(game):
+    words_list = game.words_list
+    freqs = st.get_letter_freq(words_list)
+    scores = st.score_words_list(words_list, freqs)
+    assert len(scores) == len(words_list)
+    assert scores == st.DEFAULT_SCORES
+
+
+def test_cheat():
+    game = Game()
+    solved = st.cheat(game)
+    assert solved.game_over
+    assert solved.results[-1] == 5 * ["green"]
+    assert solved.greens[-1] == [char for char in game.target_word]
+    assert solved.move_counter == 1
+
+
+def test_cheat_more():
+    game = Game()
+    solved = st.cheat_more(game)
+    assert solved.game_over
+    assert solved.move_counter == 0
+
+
+def test_linear_select():
+    game = Game()
+    solved = st.linear_select(game)
+    assert solved.game_over
+    assert solved.words_list[solved.move_counter - 1] == game.target_word
+
+
+def test_random_select():
+    game = Game()
+    solved = st.random_select(game)
+    assert solved.game_over
+    assert solved.move_counter > 10
+
+
+def test_guess_by_freq():
+    game = Game(target_word="apple")
+    solved = st.guess_by_freq(game)
+    assert solved.game_over
+    assert solved.guesses == ["slate", "cable", "agile", "apple"]
+    assert solved.move_counter == 4
+
+
+def test_guess_by_updating_freq():
+    game = Game(target_word="apple")
+    solved = st.guess_by_updating_freq(game)
+    assert solved.game_over
+    print(solved.guesses)
+    assert solved.guesses == ["slate", "maple", "apple"]
+    assert solved.move_counter == 3
+
+
+def test_run_trial():
+    try_ten = st.run_trial(st.cheat, 10)
+    assert len(try_ten) == 10
+    score, error = st.evaluate_trial(try_ten)
+    assert score == 1.0
+    assert error == 0.0
+
+    all_words = st.run_trial(st.cheat_more)
+    assert len(all_words) == 2315
+    score, error = st.evaluate_trial(all_words)
+    assert score == 0.0
+    assert error == 0.0
+
+
+def test_all():
+    for strategy in st.STRATEGIES
+        trial = st.run_trial(strategy)
+        score, _ = st.evaluate_trial(trial)
+        print("For strategy {}".format(strategy.__name__))
+        print("    Average solve in: {:.2f}\n".format(score))
+
--- wordle_strategy_evaluation/strategy.py
+++ wordle_strategy_evaluation/strategy.py
@@ -1,3 +1,11 @@
+import string
+import random
+import math
+from statistics import mean, stdev
+
+from wordle_strategy_evaluation.game import DEFAULT_WORDS_LIST, Game
+
+
 def filter_greens(game, greens, words_list=None):
     if words_list is None:
         words_list = game.words_list
@@ -39,25 +47,106 @@ def filter_all(game, words_list=None):
     if words_list is None:
         words_list = game.words_list
 
+    if game.move_counter == 0:
+        greens = yellows = greys = 5 * [""]
+    else:
+        greens, yellows, greys = (game.greens[-1], game.yellows[-1], game.greys[-1])
+
     filtered = words_list
-    filtered = filter_greens(game, game.greens[-1], filtered)
-    filtered = filter_yellows(game, game.yellows[-1], filtered)
-    filtered = filter_greys(game, game.greys[-1], filtered)
+    filtered = filter_greens(game, greens, filtered)
+    filtered = filter_yellows(game, yellows, filtered)
+    filtered = filter_greys(game, greys, filtered)
     return filtered
 
 
-def _add_to_dict(dictionary, character):
-    if character not in dictionary.keys():
-        dictionary[character] = 1
-    else:
-        dictionary[character] += 1
-    return dictionary
-
-
 def get_letter_freq(words_list):
-    position_counts = [{} for _ in range(5)]
+    position_counts = [{char: 0 for char in string.ascii_lowercase} for _ in range(5)]
     for word in words_list:
         for i, char in enumerate(word):
-            position_counts[i] = _add_to_dict(position_counts[i], char)
-
+            position_counts[i][char] += 1  # _add_to_dict(position_counts[i], char)
     return position_counts
+
+
+def score(word, position_counts):
+    score = 0
+    for char, counts in zip(word, position_counts):
+        score += counts[char]
+    return score
+
+
+def _sort_dict(dictionary):
+    return {
+        k: v
+        for k, v in sorted(dictionary.items(), key=lambda item: item[1], reverse=True)
+    }
+
+
+def score_words_list(words, position_counts):
+    return _sort_dict({word: score(word, position_counts) for word in words})
+
+
+def cheat(game):
+    game.guess(game.target_word)
+    return game
+
+
+def cheat_more(game):
+    game.game_over = True
+    return game
+
+
+def linear_select(game):
+    for guess in DEFAULT_WORDS_LIST:
+        game.guess(guess)
+        if game.game_over:
+            return game
+
+
+def random_select(game):
+    while not game.game_over:
+        guess = random.choice(game.words_list)
+        game.guess(guess)
+    return game
+
+
+def guess_by_freq(game):
+    filtered = DEFAULT_WORDS_LIST
+    while not game.game_over:
+        filtered = filter_all(game)
+        guess = list(score_words_list(filtered, DEFAULT_FREQS).keys())[0]
+        game.guess(guess)
+    return game
+
+
+def guess_by_updating_freq(game):
+    filtered = DEFAULT_WORDS_LIST
+    while not game.game_over:
+        filtered = filter_all(game)
+        freqs = get_letter_freq(filtered)
+        guess = list(score_words_list(filtered, freqs).keys())[0]
+        game.guess(guess)
+    return game
+
+
+def run_trial(strategy, nruns=None):
+    if not nruns:
+        return [strategy(Game(target_word=word)) for word in DEFAULT_WORDS_LIST]
+    else:
+        return [strategy(Game()) for _ in range(nruns)]
+
+
+def evaluate_trial(trial, stdevs=2):
+    scores = [game.move_counter for game in trial]
+    return mean(scores), stdevs * stdev(scores) / math.sqrt(len(scores))
+
+
+DEFAULT_FREQS = get_letter_freq(DEFAULT_WORDS_LIST)
+DEFAULT_SCORES = score_words_list(DEFAULT_WORDS_LIST, DEFAULT_FREQS)
+STRATEGIES = [
+    cheat,
+    cheat_more,
+    linear_select,
+    random_select,
+    guess_by_freq,
+    guess_by_updating_freq,
+]
